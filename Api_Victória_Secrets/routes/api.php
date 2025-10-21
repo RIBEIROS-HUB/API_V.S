719acdb17960316c1515bc44839c92673ca8217a
RIBEIROS-HUB <daniel.santana64@etec.sp.gov.br>
2025-10-20 21:37:36 -0300

    Replace apiResource with explicit route definitions
    Alteração de Rota DE API

--- Api_Victória_Secrets/routes/api.php
+++ Api_Victória_Secrets/routes/api.php
@@ -4,4 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::apiResource('produtos', ProdutoController::class);
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
+Route::put('/produtos/{id}', [ProdutoController::class, 'update']);
+Route::delete('/produtos/{id}', [ProdutoController::class, 'destroy']);
+
